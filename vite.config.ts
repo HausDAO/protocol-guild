f6215cd543cbaf32e61098a62e55a8eba58f89d1
dekanbro <dekanbrown@odyssy.io>
2023-07-25 13:22:37 -0600

    update build to single file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile"
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   Object.assign(process.env, loadEnv(mode, process.cwd()));
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), viteSingleFile()],
+    
     define: {
       "process.env": {
         NX_RIVET_KEY: process.env.VITE_RIVET_KEY,
