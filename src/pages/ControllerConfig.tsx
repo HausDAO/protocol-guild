f472902dd5e3bc7d80534aa5697bcaf9348b6b7f
dekanbro <dekanbrown@odyssy.io>
2023-07-28 14:31:27 -0600

    start on dialog

--- src/pages/ControllerConfig.tsx
+++ src/pages/ControllerConfig.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-import { AddressDisplay, ParLg, SingleColumnLayout } from "@daohaus/ui";
+import {
+  AddressDisplay,
+  Dialog,
+  DialogContent,
+  ParLg,
+  SingleColumnLayout,
+  useToast,
+} from "@daohaus/ui";
 import { FormBuilder } from "@daohaus/form-builder";
 import { APP_FORM } from "../legos/forms";
 import { TARGETS } from "../targetDao";
@@ -8,8 +15,15 @@ import { HAUS_RPC } from "./Home";
 import { useMemberRegistry } from "../hooks/useRegistry";
 import { AddressKeyChain, ValidNetwork } from "../utils/createContract";
 import { Keychain } from "@daohaus/keychain-utils";
+import { handleErrorMessage } from "@daohaus/utils";
+import { useTxBuilder } from "@daohaus/tx-builder";
 
 export const ControllerConfig = () => {
+  const { fireTransaction } = useTxBuilder();
+  const { errorToast, defaultToast, successToast } = useToast();
+  const [isTxLoading, setIsTxLoading] = React.useState(false);
+  const [isSuccess, setIsSuccess] = React.useState(false);
+
   const daochain = TARGETS.NETWORK_ID as ValidNetwork;
   const { isIdle, isLoading, error, data, refetch } = useMemberRegistry({
     registryAddress: TARGETS.REGISTRY_ADDRESS,
@@ -20,26 +34,56 @@ export const ControllerConfig = () => {
   if (isLoading) return <ParLg>Loading...</ParLg>;
 
   return (
-    <SingleColumnLayout title="0xSplits Controller">
-      <ParLg>
-        Owner:{" "}
-        {data?.owner.toLowerCase() == TARGETS.SAFE_ADDRESS.toLowerCase() &&
-          "DAO"}
-      </ParLg>
-      <AddressDisplay
-        address={data?.owner as string}
-        truncate
-        copy
-        explorerNetworkId={daochain as keyof Keychain}
-      />
-      <ParLg>TODO: Transfer Control</ParLg>
-      <ParLg>TODO: Cancel transfer</ParLg>
-      <ParLg>TODO: manage control on foreignRegistries</ParLg>
+    <>
+      <SingleColumnLayout title="0xSplits Controller">
+        <ParLg>
+          Registry Owner:{" "}
+          {data?.owner.toLowerCase() == TARGETS.SAFE_ADDRESS.toLowerCase() &&
+            "DAO"}
+        </ParLg>
+        <AddressDisplay
+          address={data?.owner as string}
+          truncate
+          copy
+          explorerNetworkId={daochain as keyof Keychain}
+        />
+        <ParLg>TODO: Split transfer status</ParLg>
+        <ParLg>TODO: Transfer Control</ParLg>
+        <ParLg>TODO: Cancel transfer</ParLg>
+        <ParLg>TODO: manage control on foreignRegistries</ParLg>
+        <ParLg>TODO: add success dialog and direct to DAO</ParLg>
 
-      <FormBuilder
-        form={APP_FORM.ACCEPT_CONTROLL}
-        targetNetwork={TARGETS.NETWORK_ID}
-      />
-    </SingleColumnLayout>
+
+        <FormBuilder
+          form={APP_FORM.ACCEPT_CONTROLL}
+          targetNetwork={TARGETS.NETWORK_ID}
+          lifeCycleFns={{
+            onTxError: (error) => {
+              const errMsg = handleErrorMessage({
+                error,
+              });
+              errorToast({ title: "Trigger Failed", description: errMsg });
+              setIsTxLoading(false);
+            },
+            onTxSuccess: () => {
+              defaultToast({
+                title: "Trigger Success",
+                description: "Proposal has been submitted",
+              });
+              setIsTxLoading(false);
+              setIsSuccess(true);
+            },
+          }}
+        />
+      </SingleColumnLayout>
+      {isSuccess && (
+        <Dialog>
+          <DialogContent title="Success">
+            <ParLg>Success!</ParLg>
+            <ParLg>Go to DAO and vote on proposal</ParLg>
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
   );
 };
