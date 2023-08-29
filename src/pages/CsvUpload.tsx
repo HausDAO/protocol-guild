ce0d7cd40f83bc7480ef55be58c8747c48e2517c
dekanbro <dekanbrown@odyssy.io>
2023-08-29 11:46:27 -0600

    some cleanup on csv upload controls

--- src/pages/CsvUpload.tsx
+++ src/pages/CsvUpload.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 
-import { Divider, ParLg, ParMd, SingleColumnLayout, WarningText } from "@daohaus/ui";
+import {
+  Divider,
+  HelperText,
+  ParLg,
+  ParMd,
+  SingleColumnLayout,
+  WarningText,
+} from "@daohaus/ui";
 
 import { CsvUploader } from "../components/CsvUploader";
 import { Member, StagingMember } from "../types/Member.types";
@@ -13,25 +20,24 @@ import styled from "styled-components";
 
 const StyledDivider = styled(Divider)`
   margin-bottom: 2rem;
-`
+`;
 
 export const CsvUpload = () => {
-
   const { isIdle, isLoading, error, data, refetch } = useMemberRegistry({
     registryAddress: TARGETS.REGISTRY_ADDRESS,
     chainId: TARGETS.NETWORK_ID,
     rpcs: HAUS_RPC,
   });
-  
-  const [memberList, setMemberList] = useState<Member[] | null>(null);  
+
+  const [memberList, setMemberList] = useState<Member[] | null>(null);
   const [stageMemberList, setStageMemberList] = useState<StagingMember[]>([]);
 
-
   useEffect(() => {
-    
     if (data?.members && memberList) {
       const stagingList: StagingMember[] = memberList.map((member) => {
-        const isNewMember = !data.members.find((m) => m.account === member.account);        
+        const isNewMember = !data.members.find(
+          (m) => m.account === member.account
+        );
         return {
           account: member.account,
           activityMultiplier: member.activityMultiplier,
@@ -40,27 +46,42 @@ export const CsvUpload = () => {
         };
       });
       setStageMemberList(stagingList);
-
     }
   }, [data, memberList]);
 
+  const handleSuccess = () => {
+    setMemberList(null);
+    setStageMemberList([]);
+    refetch();
+  };
 
   return (
     <SingleColumnLayout>
-
-      <ParLg>CSV Upload format:</ParLg>
-      <ParMd>Format and 1st row header: address, modifier, startdate</ParMd>
+      <ParMd>
+        Upload a CSV file with the following columns: address, modifier,
+        startDate
+      </ParMd>
       <StyledDivider />
 
       <CsvUploader setMemberList={setMemberList} />
 
-      
       {stageMemberList.length > 0 && (
-      <>
-      <MemberImportTable memberList={stageMemberList} />
-      <ComboMemberProposal onSuccess={() => console.log("success")} stageMemberList={stageMemberList} />
-      </>)}
-
+        <>
+          <ParLg>count: {stageMemberList.length}</ParLg>
+          <MemberImportTable memberList={stageMemberList} />
+          {stageMemberList.length < 50 ? (
+            <ComboMemberProposal
+              onSuccess={handleSuccess}
+              stageMemberList={stageMemberList}
+            />
+          ) : (
+            <HelperText>
+              Too many members to create a proposal. Please upload a smaller
+              list (less than 50).
+            </HelperText>
+          )}
+        </>
+      )}
     </SingleColumnLayout>
   );
 };
